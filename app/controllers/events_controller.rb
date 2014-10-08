75b526fb082bcd2e32bbae2e11d7d8f87d16d6f1
linda lizeth <lindalizethpereira@gmail.com>
2014-10-08 10:57:35 -0500

    listo cronograma

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,51 +1,29 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /events
-  # GET /events.json
   def index
     @events = Event.all
   end
 
-  # GET /events/1
-  # GET /events/1.json
   def show
   end
 
-  # GET /events/new
   def new
     @event = Event.new
-
   end
 
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
-    
     render action: :new unless @event.save
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+      render action: :edit unless @event.update_attributes(event_params) 
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
@@ -64,4 +42,5 @@ class EventsController < ApplicationController
     def event_params
       params.require(:event).permit(:title, :description, :start, :end)
     end
+
 end
